2975990286a706bfe8fbf8cc9d0c901e1af683f4
juanem-ar <juanemlopez@gmail.com>
2023-10-16 16:54:25 -0300

    modulo 3 completado y examen rendido

--- modulo3/TablaComunCTE.sql
+++ modulo3/TablaComunCTE.sql
@@ -9,3 +9,26 @@ as(
 	where SalesOrderID=43659
 )
 select * from Sales_CTE
+
+
+/*
+-- Laboratorio
+
+-- use AdventureWorks2019
+
+5. Crear una CTE con las órdenes de
+venta
+Tablas: Sales.SalesOrderHeader
+Campos: SalesPersonID, SalesOrderID,
+OrderDate
+
+with Sales_CTE (SalesPersonID,SalesOrderID,OrderDate)
+as(
+	select 
+		SalesPersonID, 
+		SalesOrderID, 
+		OrderDate
+	from sales.SalesOrderHeader
+)
+select * from Sales_CTE
+*/
--- modulo3/TablasTemporalesConBulkInsert.sql
+++ modulo3/TablasTemporalesConBulkInsert.sql
@@ -1,5 +1,6 @@
 -- use Northwind
 -- Creando una tabla temporal local
+/*
 select
 p.ProductID id,
 s.CompanyName proveedor,
@@ -31,3 +32,16 @@ p.UnitPrice*0.21 IVA,
 p.UnitPrice + p.UnitPrice*0.21 PrecioIVAIncluido
 into Precios
 from Products p join Suppliers s on p.SupplierID = s.SupplierID
+
+-- Laboratorio
+-- use AdventureWorks2019
+1. Clonar estructura y datos de los campos nombre,
+color y precio de lista de la tabla Production.Product
+en una tabla llamada #Productos.
+Tablas: Production.Product
+Campos: Name, ListPrice, Colorselect	pp.name nombre,	pp.color,	pp.ListPrice precioLista	into #Productosfrom Production.Product ppselect * from #Productos2. Clonar solo estructura de los campos identificador,
+nombre y apellido de la tabla Person.Person en una
+tabla llamada #Personas
+Tablas: Person.Person
+Campos: BusinessEntityID, FirstName, LastNameselect	pp.BusinessEntityID,	pp.FirstName,	pp.LastName	into #Personasfrom Person.Person ppselect * from #Personas3. Eliminar si existe la tabla #Productos
+Tablas: #Productosdrop table if exists #Productosselect * from #Productos*/
